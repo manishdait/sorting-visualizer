4d4065778ada9bd106a076dffb8c0ff187b8f3e8
Manish Dait <daitmanish88@gmail.com>
2023-07-04 19:11:37 +0530

    2023-07-04:build

--- docs/assets/audio/Sound.ts
+++ docs/assets/audio/Sound.ts
@@ -1,18 +1,26 @@
 let audio = new Audio();
+let isMuted: boolean = false;
 export async function playSucess(){
     audio.src = "assets/audio/complete.mp3";
     audio.load();
-    audio.play();
+    if(!isMuted)
+      audio.play();
   }
 
 export async function playSwap() {
   audio.src = "assets/audio/swap.mp3";
   audio.load();
-  audio.play();
+  if(!isMuted)
+    audio.play();
 }
 
 export async function playFind(){
   audio.src = "assets/audio/find.mp3";
   audio.load();
-  audio.play();
+  if(!isMuted)
+    audio.play();
+}
+
+export function setMute(val: boolean) {
+  isMuted = val;
 }
